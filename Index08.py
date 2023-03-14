ebbd376c30540f716f6e92626fd19e87e1df3add
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-14 15:03:20 +0500

    Index08

--- Index08.py
+++ Index08.py
@@ -6,5 +6,22 @@ def main(s):
     Returns:
         int: answer
     """
-    return
-        
+    if len(s)<=5:
+        if len(s)>=1 and s[0]=="*":
+            return 0
+
+        elif len(s)>=2 and s[1]=="*":
+            return 1
+
+        elif len(s)>=3 and s[2]=="*":
+            return 2
+
+        elif len(s)>=4 and s[3]=="*":
+            return 3
+
+        elif len(s)==5 and s[4]=="*":
+            return 4
+        else:
+            return False
+
+print(main("kej*d"))
